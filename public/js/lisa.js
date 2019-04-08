fa81ecda899af2ef370d10cef58fd2d75873d411
Lisa Simpson <lisamaree02@Lisas-MacBook-Pro.local>
2019-04-08 16:03:43 +1200

    renamed scss to sass and gruntfile changes

--- public/js/lisa.js
+++ public/js/lisa.js
@@ -1,1 +1,18 @@
-console.log("hey lisa");
+console.log("working 4pm");
+// $.ajax({
+
+//     dataType: 'jsonp',
+//     url: 'http://behance.net/v2/users?api_key=n8cdQnwhaNoOOeV04JAR1P3Uhz7PQ8p7',
+//     type: 'GET',
+//     success: function (users) {
+//         console.log(users);
+//         console.log(users.users.length);
+//         for (var i = 0; i < 3; i++){
+//             console.log(users.users[i].first_name);
+//         }
+//     },
+//     error: function (error) {
+
+//         console.log(error);
+//     }
+// });
