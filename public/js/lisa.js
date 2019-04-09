022f1a88d9f4155e8bcf20cff48d12d95a64b938
Lisa Simpson <lisamaree02@Lisas-MacBook-Pro.local>
2019-04-09 15:47:48 +1200

    data displaying, added footer

--- public/js/lisa.js
+++ public/js/lisa.js
@@ -1,20 +1,35 @@
 console.log("working");
+var desName = ['matiascorea', 'lucaviola', 'raewynbrandon'];
+
+for (var i = 0; i < desName.length; i++){
+console.log(desName);
+
+
 $.ajax({
     dataType: 'jsonp',
-    url: 'https://api.behance.net/v2/users//matiascorea/projects?client_id=n8cdQnwhaNoOOeV04JAR1P3Uhz7PQ8p7',
+    url: 'https://api.behance.net/v2/users/'+ desName[i] +'?client_id=n8cdQnwhaNoOOeV04JAR1P3Uhz7PQ8p7',
     // url: 'http://behance.net/v2/users?api_key=n8cdQnwhaNoOOeV04JAR1P3Uhz7PQ8p7',
     type: 'GET',
-    success: function (behance__data) {
-        console.log(behance__data);
-        // console.log(behance__data.users.length);
-        //console.log(behance__data.users.matiascorea);
+    success: function (behance_data) {
+        console.log(behance_data);
+        // console.log(behance_data.users.length);
+        console.log(behance_data.user.first_name);
+        console.log(behance_data.user.last_name);
+        console.log(behance_data.user.occupation);
+        console.log(behance_data.user.fields);
+        console.log(behance_data.user.images[138]);
 
-        for (var i = 0; i < 3; i++){
-            console.log(behance__data.users[i].first_name);
-        }
+       document.getElementById("des__details").innerHTML += '<div class="cont__designer"><p class="designer__h2--f">' + behance_data.user.first_name + ' ' + behance_data.user.last_name +'</p><div>' + 
+        '<p class="designer__h2--f">' + behance_data.user.occupation + '</p>' +
+        '<img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo">';
+       
+       //document.getElementById("des__details--img").innerHTML += '<img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo" style="width:200px; height:200px;">';
     },
     error: function (error) {
 
         console.log(error);
     }
 });
+
+}
+//loop is closed
