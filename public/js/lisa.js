0168a233c09865bb71138aa761331c155ac7f2b0
Lisa Simpson <lisamaree02@Lisas-MacBook-Pro.local>
2019-04-10 10:37:56 +1200

    js working, working on style

--- public/js/lisa.js
+++ public/js/lisa.js
@@ -1,4 +1,4 @@
-console.log("working");
+// console.log("working");
 var desName = ['matiascorea', 'lucaviola', 'raewynbrandon'];
 
 for (var i = 0; i < desName.length; i++){
@@ -10,6 +10,7 @@ $.ajax({
     url: 'https://api.behance.net/v2/users/'+ desName[i] +'?client_id=n8cdQnwhaNoOOeV04JAR1P3Uhz7PQ8p7',
     // url: 'http://behance.net/v2/users?api_key=n8cdQnwhaNoOOeV04JAR1P3Uhz7PQ8p7',
     type: 'GET',
+    async: false,
     success: function (behance_data) {
         console.log(behance_data);
         // console.log(behance_data.users.length);
@@ -19,10 +20,12 @@ $.ajax({
         console.log(behance_data.user.fields);
         console.log(behance_data.user.images[138]);
 
-       document.getElementById("des__details").innerHTML += '<div class="cont__designer"><p class="designer__h2--f">' + behance_data.user.first_name + ' ' + behance_data.user.last_name +'</p><div>' + 
-        '<p class="designer__h2--f">' + behance_data.user.occupation + '</p>' +
-        '<img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo">';
+       document.getElementById("des__details").innerHTML += '<div class="cont__designer row text-centre"><p class="designer__h2--f col-12">' + behance_data.user.first_name + ' ' + behance_data.user.last_name +'</p><div>' + 
+        '<p class="designer__h2--f">' + behance_data.user.occupation + '</p>' 
+        //+
+        //'<div class="col-lg-6"><img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo"></div>';
        
+       document.getElementById("des__details--img").innerHTML += '<img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo"';
        //document.getElementById("des__details--img").innerHTML += '<img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo" style="width:200px; height:200px;">';
     },
     error: function (error) {
