9483f38a20a7ad269f1dab9df970526bc3eb1770
Lisa Simpson <lisamaree02@Lisas-MacBook-Pro.local>
2019-04-10 23:20:42 +1200

    working on js

--- public/js/lisa.js
+++ public/js/lisa.js
@@ -1,5 +1,8 @@
 // console.log("working");
 var desName = ['matiascorea', 'lucaviola', 'raewynbrandon'];
+var firstDes = '';
+var j = 0;
+var len = desName.length;
 
 for (var i = 0; i < desName.length; i++){
 console.log(desName);
@@ -20,13 +23,20 @@ $.ajax({
         console.log(behance_data.user.fields);
         console.log(behance_data.user.images[138]);
 
-       document.getElementById("des__details").innerHTML+= '<div class="cont__designer row text-centre"><p class="designer__h2--f col-12">' + behance_data.user.first_name + ' ' + behance_data.user.last_name +'</p>' + 
+        // for (var j = 0; j < desName[i].length; j++){
+        // }
+
+        document.getElementById("des__details").innerHTML+= '<div class="row cont__designer d-flex justify-content-center"><p class="designer__h2--f col">' + behance_data.user.first_name + ' ' + behance_data.user.last_name +'</p>' + 
         '<p class="designer__h2--f">' + behance_data.user.occupation + '</p></div>';
         //+
         //'<div class="col-lg-6"><img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo"></div>';
        
-       document.getElementById("des__details--img").innerHTML += '<img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo"';
+       document.getElementById("des__details--img").innerHTML += '<div class="cont__des--image"><img class="img-thumbnail img__shadow col" src = "' + behance_data.user.images[138] + '" alt="profile photo"</div>';
        //document.getElementById("des__details--img").innerHTML += '<img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo" style="width:200px; height:200px;">';
+    
+    
+    
+    
     },
     error: function (error) {
 
