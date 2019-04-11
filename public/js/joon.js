93a4ffb438a1d934d240fbfaef48369591966734
Joon Yoon <myexistance@hotmail.com>
2019-04-11 12:57:33 +0000

    username and loadmore for divs working

--- public/js/joon.js
+++ public/js/joon.js
@@ -25,42 +25,42 @@ for (i=0; i<3; i++){
 
             var project = data.projects[j];
              console.log(project);
-             // if (j>0){
-             //   $("#project2, #project3").hide();
-             //  $("#more").click(function(){
-             //    $("#project2, #project3").show().
-             //  })
-                  // }
+             if (j>0){
+               $('#project2, #project3').hide();
+              $('#more').click(function(){
+                $('#project2, #project3').show();
+              })
+                  }
 
 
              if (j==0) {
-              container.append('<div id="project1" style="height: 75em; width: 100%; border: blue 1px solid;"></div>')
+              container.append('<div align="center" id="project1" style="height: 75em; width: 100%; border: blue 1px solid;"></div>')
 
 
             var   projContainer1 = document.getElementById('project1')
                  projContainer1.innerHTML +=('<div style="height: 1.825em; width: 300px;" class="project__item">'+project.owners[0].display_name+'</div>')
                  projContainer1.innerHTML +=('<div style="height: 1.825em; width: 400px;" class="project__item">'+project.name+'</div>')
-                  projContainer1.innerHTML +=('<div style="height: 21.5em; width: 300px;" class="project__item">' + '<img src= '+ project.covers[404] +' >'+'</div>')
+                  projContainer1.innerHTML +=('<div style="height: 21.5em; width: 250px;" class="project__item">' + '<img src= '+ project.covers[404] +' >'+'</div>')
 
               console.log(projContainer1);
 
 
              } else if (j==1){
 
-              container.append('<div id="project2" style="height: 75em; width: 100%; border: red 1px solid;"></div>')
+              container.append('<div align="center" id="project2" style="height: 75em; width: 100%; border: red 1px solid;"></div>')
               var projContainer2 = document.getElementById('project2')
               projContainer2.innerHTML +=('<div style="height: 1.825em; width: 300px;" class="project__item">'+project.owners[0].display_name+'</div>')
                 projContainer2.innerHTML +=('<div style="height: 1.825em; width: 300px;" class="project__item">'+project.name+'</div>')
-                 projContainer2.innerHTML +=('<div style="height: 21.5em; width: 300px;" class="project__item">' + '<img src= '+ project.covers[404] +' >'+'</div>')
+                 projContainer2.innerHTML +=('<div style="height: 21.5em; width: 250px;" class="project__item">' + '<img src= '+ project.covers[404] +' >'+'</div>')
                   console.log(projContainer2);
 
             } else if (j==2){
-              container.append('<div id="project3" style="height: 75em; width: 100%; border: green 1px solid; "></div>')
+              container.append('<div align="center" id="project3" style="height: 75em; width: 100%; border: green 1px solid; "></div>')
               var projContainer3 = document.getElementById('project3')
 
               projContainer3.innerHTML +=('<div style="height: 1.825em; width: 300px;" class="project__item">'+project.owners[0].display_name+'</div>')
                 projContainer3.innerHTML +=('<div style="height: 1.825em; width: 300px;" class="project__item">'+project.name+'</div>')
-                 projContainer3.innerHTML +=('<div style="height: 21.5em; width: 300px;" class="project__item">' + '<img src= '+ project.covers[404] +' >'+'</div>')
+                 projContainer3.innerHTML +=('<div style="height: 21.5em; width: 250px;" class="project__item">' + '<img src= '+ project.covers[404] +' >'+'</div>')
 
             console.log(projContainer3);
             }
