ee793bd5a31974f9b63bbf7227480dfc0c5115fa
Lisa Simpson <lisamaree02@Lisas-MacBook-Pro.local>
2019-04-12 12:26:08 +1200

    updating so we can merge

--- public/js/joon.js
+++
@@ -1,1 +0,0 @@
-console.log("hey Joon it's monday");
--- public/js/lisa.js
+++ public/js/lisa.js
@@ -1,8 +1,9 @@
 // console.log("working");
 var desName = ['matiascorea', 'lucaviola', 'raewynbrandon'];
-var firstDes = '';
-var j = 0;
+var j = desName[0];
+console.log(j);
 var len = desName.length;
+var firstDes = ['matiascorea'];
 
 for (var i = 0; i < desName.length; i++){
 console.log(desName);
@@ -23,15 +24,32 @@ $.ajax({
         console.log(behance_data.user.fields);
         console.log(behance_data.user.images[138]);
 
-        // for (var j = 0; j < desName[i].length; j++){
-        // }
+        // if (behance_data.user.first_name < 10) {
+        //     des1 = "Matias Corea";
+        //   } else if (behance_data.user.first_name < 8) {
+        //     des2 = "Luka Viola";
+        //   }
+        //   else {
+        //     des3 = "Raewyn Brandon";
+        //   }
+        // console.log(des1)
+        // console.log(des2)
+        // console.log(des3)
 
-        document.getElementById("des__details").innerHTML+= '<div class="row cont__designer d-flex justify-content-center"><p class="designer__h2--f col">' + behance_data.user.first_name + ' ' + behance_data.user.last_name +'</p>' + 
-        '<p class="designer__h2--f">' + behance_data.user.occupation + '</p></div>';
-        //+
-        //'<div class="col-lg-6"><img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo"></div>';
+        //TRYING TO GET FIRST NAME INDIVIDUALLY FOR STYLING - COLUMNS to sit right
+
+        // var desObj, x;
+        // desObj = {"name":"John", "age":30, "car":null};
+        // x = desObj.name;
+        // document.getElementById("demo").innerHTML = x;
+
+
+        document.getElementById("des__details").innerHTML+= '<div class="user-row"><div class="text-row"><p class="designer__h2--f">' + behance_data.user.first_name + ' ' + behance_data.user.last_name +'</p>' + 
+        '<br>'+'<p class="designer__h2--f">' + behance_data.user.occupation + '</p></div>'
+        +
+        '<div class="img-row"><img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo"></div></div>';
        
-       document.getElementById("des__details--img").innerHTML += '<div class="cont__des--image"><img class="img-thumbnail img__shadow col" src = "' + behance_data.user.images[138] + '" alt="profile photo"</div>';
+    //    document.getElementById("des__details--img").innerHTML += '<div class="cont__des--image"><img class="img-thumbnail img__shadow col" src = "' + behance_data.user.images[138] + '" alt="profile photo"</div>';
        //document.getElementById("des__details--img").innerHTML += '<img class="img-thumbnail img__shadow" src = "' + behance_data.user.images[138] + '" alt="profile photo" style="width:200px; height:200px;">';
     
     
