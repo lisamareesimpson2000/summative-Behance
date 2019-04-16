91c5be6950b9085807008eec46e3871f747aa4d9
Lexi-Jayy Critchley <845869864@bun2-206-18.local>
2019-04-16 13:17:36 +1200

    hoping this isnt broken

--- public/js/main.js
+++ public/js/main.js
@@ -1,54 +1,3 @@
-
-//LISA JS
-$("#show__section--lisa").click(function(){
-    $("#joon").hide();
-    $("#lisa").show();
-    $('html, body').animate({
-        scrollTop: $("#lisa").offset().top
-     }, 1000);
-});
-$("#show__section--joon").click(function(){
-    $("#lisa").hide();
-    $("#joon").show();
-    $('html, body').animate({
-        scrollTop: $("#joon").offset().top
-     }, 1000);
-});
-
-var desName = ['matiascorea', 'lucaviola', 'raewynbrandon'];
-var j = desName[0];
-console.log(j);
-var len = desName.length;
-var firstDes = ['matiascorea'];
-
-for (var i = 0; i < desName.length; i++){
-console.log(desName);
-
-$.ajax({
-    dataType: 'jsonp',
-    url: 'https://api.behance.net/v2/users/'+ desName[i] +'?client_id=n8cdQnwhaNoOOeV04JAR1P3Uhz7PQ8p7',
-    type: 'GET',
-    async: false,
-    success: function (behance_data) {
-        console.log(behance_data);
-        console.log(behance_data.user.first_name);
-        console.log(behance_data.user.last_name);
-        console.log(behance_data.user.occupation);
-        console.log(behance_data.user.location);
-        console.log(behance_data.user.fields);
-        console.log(behance_data.user.twitter);
-        console.log(behance_data.user.images[138]);
-        document.getElementById("section__list--designer").innerHTML+= '<div class="row__user"><div class="row__text"><h2 class="designer__h2--f">' + behance_data.user.first_name + ' ' + behance_data.user.last_name +'</h2>' +
-        '<h2 class="designer__h2--f">' + behance_data.user.occupation + '</h2>' + '<h2 class="designer__h2--f">' + behance_data.user.location +'</h2></div>' +
-        '<div class="row__img"><img class="rounded-circle ridge img__shadow clearfix" src = "' + behance_data.user.images[138] + '" alt="profile photo"></div></div>'+'<br>';
-    },
-    error: function (error) {
-        console.log(error);
-    }
-});
-
-
-
 var Desname = ["matiascorea" , "lucaviola" , "raewynbrandon"];
 var stats = [
     {
@@ -79,10 +28,11 @@ var stats = [
 
 var j = 0;
 for ( var i = 0 ; i < Desname.length; i++) {
+    console.log(Desname[i]);
 
 
 $.ajax({
-
+        
         dataType: 'jsonp',
         url: 'http://behance.net/v2/users/' + Desname[i] + '?api_key=JoI9j5mk8tEfLB81PQeEMKhDSTjVNewT',
         type: 'GET',
@@ -102,12 +52,8 @@ $.ajax({
             //return(apiData.user.stats.followers);
             console.log(stats[j].first_name,stats[j].followers, stats[j].following, stats[j].appreciations, stats[j].views, stats[j].comments);
             console.log(j);
-    j += 1;
-
-
-
-
-
+    j += 1;        
+            
 
 
         },
@@ -121,17 +67,16 @@ google.charts.load('current', {'packages':['corechart', 'controls']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-
-var data = new google.visualization.DataTable();
-data.addColumn('string','Name');
-       data.addColumn('number','Followers');
-       for (var i = 0; i < stats.length; i++) {
-         console.log(stats[i].first_name, stats[i].followers)
-          data.addRow([
-                    stats[i].first_name,
-                    stats[i].followers
-                    ]);
-          } //arraytodatatable
+  var data = new google.visualization.DataTable();
+    data.addColumn('string','Name');
+    data.addColumn('number','Followers');
+      for (var i = 0; i < stats.length; i++) {
+        console.log(stats[i].first_name, stats[i].followers)
+        data.addRow([
+          stats[i].first_name,
+          stats[i].followers
+        ]);
+      } //arraytodatatable
 
 var options = {
 title: 'behance followers',
@@ -141,7 +86,7 @@ chartArea:{
   width:'70%',
   height:'90%'
 },
-is3D: true,
+is3D: true, 
 pieSliceBorderColor:  '#b5d3dd',
 backgroundColor: {
   fill: 'blue'
@@ -202,9 +147,9 @@ dashboard.draw(data);
 //   document.getElementById("dashboard_div").style.display="block";
 // });
 // $("#dashboard_div").hide();
-$('#infobtn').click(function(){
-  $("#dashboard_div").toggle();
-});
+// $('#infobtn').click(function(){
+//   $("#dashboard_div").toggle();
+// });
 // $(document).ready(function(){
 //   $('#infobtn').click(function(){
 //     $('#dashboard_div').show();
@@ -213,223 +158,3 @@ $('#infobtn').click(function(){
 //     $('#dashboard_div').hide();
 //   });
 // });
-
-
-$(document).ready(function(){
-  $('#infobtn').click(function(){
-    $('#myModal').show();
-    $('#dashboard_div').show();
-  });
-  $('.close').click(function(){
-    $('#myModal').hide();
-    $('#dashboard_div').hide();
-  });
-});
-
-
-console.log("hey Joon it's monday");
-}
-//loop is closed
-
-
-var options = {
-title: 'behance followers'
-};
-
-var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-chart.draw(data, options);
-$(document).ready(function() {
-
-//user 1 matiascorea
-  $.ajax({
-    url: 'https://api.behance.net/v2/users/matiascorea/projects?client_id=dSoSYYcQQTsMHJ9O5hWkN6gns4aV5gOM',
-    dataType: "jsonp",
-    type: "GET",
-    success: function(data){
-
-    console.log(data);
-       for (var j= 0; j < 3; j++) {
-
-            var project = data.projects[j];
-             console.log(project);
-
-
-
-
-             if (j==0) {
-              userContainer1.append('<div id="project1"></div>');
-
-              var  projContainer1 = document.getElementById('project1')
-                   projContainer1.innerHTML +=('<div class="project__item">'+project.owners[0].first_name + " " + project.owners[0].last_name+'</div>')
-                   projContainer1.innerHTML +=('<div class="project__item">'+project.name+'</div>')
-                   projContainer1.innerHTML +=('<div class="img__item">' + '<img class="shadow-float" src= '+ project.covers[404] +' >'+'</div>')
-                   projContainer1.innerHTML +=('<button type="button" id="more1" class="load__button"> Load More </button>');
-
-              console.log(projContainer1);
-
-
-             } else if (j==1){
-
-            userContainer1.append('<div id="project2"></div>')
-              var projContainer2 = document.getElementById('project2')
-                  projContainer2.innerHTML +=('<div class="project__item">'+project.owners[0].first_name + " " + project.owners[0].last_name+'</div>')
-                  projContainer2.innerHTML +=('<div class="project__item">'+project.name+'</div>')
-                  projContainer2.innerHTML +=('<div class="img__item">' + '<img class="shadow-float" src= '+ project.covers[404] +' >'+'</div>')
-
-              console.log(projContainer2);
-
-            } else if (j==2){
-              userContainer1.append('<div id="project3"></div>')
-              var projContainer3 = document.getElementById('project3')
-                  projContainer3.innerHTML +=('<div class="project__item">'+project.owners[0].first_name + " " + project.owners[0].last_name+'</div>')
-                  projContainer3.innerHTML +=('<div class="project__item">'+project.name+'</div>')
-                  projContainer3.innerHTML +=('<div class="img__item">' + '<img class="shadow-float" src= '+ project.covers[404] +' >'+'</div>')
-
-              console.log(projContainer3);
-            }
-
-
-}//for
-
-if (j>0){
-    $('#project2, #project3').hide();
-    $('#more1').click(function(){
-    $('#project2, #project3').toggle();
-  });
-      }
-
-}, //success
-    error:function(error){
-      console.log('error');
-    }
-  });
-
-  //user 2 lucaviola
-    $.ajax({
-      url: 'https://api.behance.net/v2/users/lucaviola/projects?client_id=dSoSYYcQQTsMHJ9O5hWkN6gns4aV5gOM',
-      dataType: "jsonp",
-      type: "GET",
-      success: function(data){
-
-      console.log(data);
-         for (var k= 0; k < 3; k++) {
-
-              var project = data.projects[k];
-               console.log(project);
-
-
-
-
-               if (k==0) {
-                userContainer2.append('<div id="project4"></div>');
-
-                var  projContainer4 = document.getElementById('project4')
-                     projContainer4.innerHTML +=('<div class="project__item">'+project.owners[0].first_name + " " + project.owners[0].last_name+'</div>')
-                     projContainer4.innerHTML +=('<div class="project__item">'+project.name+'</div>')
-                     projContainer4.innerHTML +=('<div class="img__item">' + '<img class="shadow-float" src= '+ project.covers[404] +' >'+'</div>')
-                     projContainer4.innerHTML +=('<button type="button" id="more2" class="load__button"> Load More </button>');
-
-                console.log(projContainer4);
-
-
-              } else if (k==1){
-
-              userContainer2.append('<div id="project5"></div>')
-                var projContainer5 = document.getElementById('project5')
-                    projContainer5.innerHTML +=('<div class="project__item">'+project.owners[0].first_name + " " + project.owners[0].last_name+'</div>')
-                    projContainer5.innerHTML +=('<div class="project__item">'+project.name+'</div>')
-                    projContainer5.innerHTML +=('<div class="img__item">' + '<img class="shadow-float" src= '+ project.covers[404] +' >'+'</div>')
-
-                console.log(projContainer5);
-
-              } else if (k==2){
-                userContainer2.append('<div id="project6"></div>')
-                var projContainer6 = document.getElementById('project6')
-                    projContainer6.innerHTML +=('<div class="project__item">'+project.owners[0].first_name + " " + project.owners[0].last_name+'</div>')
-                    projContainer6.innerHTML +=('<div class="project__item">'+project.name+'</div>')
-                    projContainer6.innerHTML +=('<div class="img__item">' + '<img class="shadow-float" src= '+ project.covers[404] +' >'+'</div>')
-
-                console.log(projContainer6);
-              }
-
-
-  }//for
-
-  if (k>0){
-      $('#project5, #project6').hide();
-      $('#more2').click(function(){
-      $('#project5, #project6').toggle();
-    });
-        }
-
-  }, //success
-      error:function(error){
-        console.log('error');
-      }
-    });
-
-    //user 3 raewynbrandon
-      $.ajax({
-        url: 'https://api.behance.net/v2/users/raewynbrandon/projects?client_id=dSoSYYcQQTsMHJ9O5hWkN6gns4aV5gOM',
-        dataType: "jsonp",
-        type: "GET",
-        success: function(data){
-
-        console.log(data);
-           for (var l= 0; l < 3; l++) {
-
-                var project = data.projects[l];
-                 console.log(project);
-
-
-
-
-                 if (l==0) {
-                  userContainer3.append('<div id="project7"></div>');
-
-                  var  projContainer7 = document.getElementById('project7')
-                       projContainer7.innerHTML +=('<div class="project__item">'+project.owners[0].first_name + " " + project.owners[0].last_name+'</div>')
-                       projContainer7.innerHTML +=('<div class="project__item">'+project.name+'</div>')
-                       projContainer7.innerHTML +=('<div class="img__item">' + '<img class="shadow-float" src= '+ project.covers[404] +' >'+'</div>')
-                       projContainer7.innerHTML +=('<button type="button" id="more3" class="load__button"> Load More </button>');
-
-
-                  console.log(projContainer7);
-
-
-                } else if (l==1){
-
-                userContainer3.append('<div id="project8"></div>')
-                  var projContainer8 = document.getElementById('project8')
-                      projContainer8.innerHTML +=('<div class="project__item">'+project.owners[0].first_name + " " + project.owners[0].last_name+'</div>')
-                      projContainer8.innerHTML +=('<div class="project__item">'+project.name+'</div>')
-                      projContainer8.innerHTML +=('<div class="img__item">' + '<img class="shadow-float" src= '+ project.covers[404] +' >'+'</div>')
-
-                  console.log(projContainer8);
-
-                } else if (l==2){
-                  userContainer3.append('<div id="project9"></div>')
-                  var projContainer9 = document.getElementById('project9')
-                      projContainer9.innerHTML +=('<div class="project__item">'+project.owners[0].first_name + " " + project.owners[0].last_name+'</div>')
-                      projContainer9.innerHTML +=('<div class="project__item">'+project.name+'</div>')
-                      projContainer9.innerHTML +=('<div class="img__item">' + '<img class="shadow-float" src= '+ project.covers[404] +' >'+'</div>')
-
-                  console.log(projContainer9);
-                }
-
-
-    }//for
-
-    if (l>0){
-        $('#project8, #project9').hide();
-        $('#more3').click(function(){
-        $('#project8, #project9').toggle();
-      });
-          }
-
-    }, //success
-        error:function(error){
-          console.log('error');
-        }
-      });
